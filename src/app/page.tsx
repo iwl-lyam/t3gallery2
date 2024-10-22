ef4576be4666d3c18a02fd5fec7960c30165fa5b
iwl-lyam <lyam.mosnier@itwithlyam.co.uk>
2024-10-22 12:16:55 +0100

    Database connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import {db} from "~/server/db";
 
 const mockUrls = [
     "https://utfs.io/f/S7crvkfPCEa3hjfB5WTmfea81QNWCowFJBdbV3njkApciIZy",
@@ -11,16 +12,19 @@ const mockImages = mockUrls.map((url, index) => ({
     url,
 }))
 
-export default function HomePage() {
-  return (
+export default async function HomePage() {
+    const posts = await db.query.posts.findMany()
+    console.log(posts)
+    return (
     <main>
-        <div className={"flex flex-wrap gap-4 items-center justify-between p-4"}>{
-            [...mockImages, ...mockImages, ...mockImages].map((image) => (
-                <div key={image.id} className="w-48">
+        <div className={"flex flex-wrap gap-4 items-center justify-between p-4"}>
+            {posts.map((post) => (<div key={post.id}>{post.name}</div>))}
+            {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+                <div key={image.id + "-" + index} className="w-48">
                     <img src={image.url} />
                 </div>
             ))
         }</div>
     </main>
-  );
+    );
 }
